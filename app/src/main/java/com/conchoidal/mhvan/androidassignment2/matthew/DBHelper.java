c3ffad4f5351f605dfb29bfb811c1c23cc796cd9
= <=>
2018-01-14 21:14:50 -0500

    Book Flight activity. If it has an error it is probably because the back button is linking to the main_menu that has not been created yet

--- app/src/main/java/com/conchoidal/mhvan/androidassignment2/matthew/DBHelper.java
+++ app/src/main/java/com/conchoidal/mhvan/androidassignment2/matthew/DBHelper.java
@@ -13,7 +13,7 @@ import android.util.Log;
 
 public class DBHelper extends SQLiteOpenHelper {
 
-    public static final String DATABASE_NAME = "hospital.db";
+    public static final String DATABASE_NAME = "flight.db";
     public static final int DATABASE_VERSION = 2;
 
     DBHelper (Context context) {
